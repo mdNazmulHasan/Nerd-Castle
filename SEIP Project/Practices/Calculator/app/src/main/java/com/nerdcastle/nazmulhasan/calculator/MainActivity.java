fdbe5786add21b61b9e99bad6588b9d46c149c75
Nazmul Hasan <najmul.hasan10@gmail.com>
2015-04-17 22:33:37 +0600

    Calculator
    calculator app

---
+++ SEIP Project/Practices/Calculator/app/src/main/java/com/nerdcastle/nazmulhasan/calculator/MainActivity.java
@@ -0,0 +1,143 @@
+package com.nerdcastle.nazmulhasan.calculator;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+    public String str ="";
+    Character op = 'q';
+    int i,num,numtemp;
+    EditText showResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showResult = (EditText)findViewById(R.id.result_id);
+    }
+    public void btn1Clicked(View v){
+        insert(1);
+
+    }
+
+    public void btn2Clicked(View v){
+        insert(2);
+
+    }
+    public void btn3Clicked(View v){
+        insert(3);
+
+    }
+    public void btn4Clicked(View v){
+        insert(4);
+
+    }
+    public void btn5Clicked(View v){
+        insert(5);
+
+    }
+    public void btn6Clicked(View v){
+        insert(6);
+    }
+    public void btn7Clicked(View v){
+        insert(7);
+
+    }
+    public void btn8Clicked(View v){
+        insert(8);
+
+    }
+    public void btn9Clicked(View v){
+        insert(9);
+
+    }
+    public void btnplusClicked(View v){
+        perform();
+        op = '+';
+
+    }
+
+    public void btnminusClicked(View v){
+        perform();
+        op = '-';
+
+    }
+    public void btndivideClicked(View v){
+        perform();
+        op = '/';
+
+    }
+    public void btnmultiClicked(View v){
+        perform();
+        op = '*';
+
+    }
+    public void btnequalClicked(View v){
+        calculate();
+
+    }
+
+    public void btnclearClicked(View v){
+        reset();
+    }
+    private void reset() {
+        // TODO Auto-generated method stub
+        str ="";
+        op ='q';
+        num = 0;
+        numtemp = 0;
+        showResult.setText("");
+    }
+    private void insert(int j) {
+        // TODO Auto-generated method stub
+        str = str+Integer.toString(j);
+        num = Integer.valueOf(str).intValue();
+        showResult.setText(str);
+
+
+    }
+    private void perform() {
+        // TODO Auto-generated method stub
+        str = "";
+        numtemp = num;
+    }
+    private void calculate() {
+        // TODO Auto-generated method stub
+        if(op == '+')
+            num = numtemp+num;
+        else if(op == '-')
+            num = numtemp-num;
+        else if(op == '/')
+            num = numtemp/num;
+        else if(op == '*')
+            num = numtemp*num;
+        showResult.setText(""+num);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
